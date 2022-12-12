93a4b73ce84dc91e0537550a982069b664bd9306
sinhvtr <sinhvtr@gmail.com>
2022-12-13 00:34:12 +0700

    first commit

---
+++ update_sach.php
@@ -0,0 +1,12 @@
+<?php
+require("header.php");
+?>
+<div class="col-sm-9">
+    <h1>Update thông tin sách</h1>
+    <form class="form form-horizontal" method="post" action="#">
+        
+    </form>
+</div>
+<?php
+require("footer.php");
+?>
